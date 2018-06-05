16f6fd165ccb090cc2fa65c2960f8ff248fa5abb
Nikola Jichev <njichev@gmail.com>
2018-06-05 14:35:01 +0300

    Add  basic number types. (#3)
    This includes integer, float, positive integer,
    negative integer and non-negative integer.
    
    For the last two - mapped positive integer with -1 *,
    and integer with abs.

--- test/stream_data/types_list.ex
+++ test/stream_data/types_list.ex
@@ -1,3 +1,10 @@
 defmodule StreamDataTest.TypesList do
   @type basic_atom :: atom()
+
+  # Numbers
+  @type basic_float() :: float()
+  @type basic_integer() :: integer()
+  @type basic_neg_integer() :: neg_integer()
+  @type basic_non_neg_integer() :: non_neg_integer()
+  @type basic_pos_integer() :: pos_integer()
 end
